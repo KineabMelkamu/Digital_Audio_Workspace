ad56a7c42521b9566af1ffea074ce4e1ca80c3fd
BestInEthiopia <melkamkt23@pwcs-edu.org>
2023-01-12 11:10:14 -0500

    me gongaga

---
+++ src/Controller.java
@@ -0,0 +1,13 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Accordion;
+
+public class Controller {
+    @FXML
+    private Accordion accordion;
+
+    public void initialize() {
+
+
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
-public class Main {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
     }
-    hjhjihjj
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(Objects.requireNonNull(getClass().getResource("Deaw.fxml")));
+        Parent root = loader.load();
+        primaryStage.setTitle("EightOhEight");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setResizable(false);
+        Controller controller = loader.getController();
+        primaryStage.show();
+    }
+
 }
